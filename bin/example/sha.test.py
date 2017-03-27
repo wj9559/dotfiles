db16f243917142183dede8416a7211e9ae7a8e98
wujian <9559wj@gmail.com>
2017-03-27 23:24:33 +0800

    timeline

--- bin/example/sha.test.py
+++ bin/example/sha.test.py
@@ -1,30 +1,37 @@
-#!/usr/bin/env python2
-from hashlib import md5, sha1, sha256, sha512
+#!/usr/bin/env python3
+from hashlib import md5, sha1, sha256, sha512, blake2b
 from random import random
 from time import time
 
-SIZE = 1000000
-rands = [`random()` for i in xrange(SIZE)]
+SIZE = 10000000
+rands = [repr(random()).encode() for i in range(SIZE)]
 
 t = time()
 hash = [md5(rand).hexdigest() for rand in rands]
-print 'md5'
-print time() - t
-print
+print('md5')
+print(time() - t)
+print()
 
 t = time()
 hash = [sha1(rand).hexdigest() for rand in rands]
-print 'sha1'
-print time() - t
-print
+print('sha1')
+print(time() - t)
+print()
 
 t = time()
 hash = [sha256(rand).hexdigest() for rand in rands]
-print 'sha256'
-print time() - t
-print
+print('sha256')
+print(time() - t)
+print()
 
 t = time()
 hash = [sha512(rand).hexdigest() for rand in rands]
-print 'sha512'
-print time() - t
+print('sha512')
+print(time() - t)
+print()
+
+t = time()
+hash = [blake2b(rand).hexdigest() for rand in rands]
+print('blake2b')
+print(time() -t)
+print()
