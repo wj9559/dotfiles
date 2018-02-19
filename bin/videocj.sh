634b5b2a4b7a37ad8ba03102d552a1247c9b6224
wujian <9559wj@gmail.com>
2018-02-19 16:27:32 +0800

    Merging the merge

--- bin/videocj.sh
+++ bin/videocj.sh
@@ -20,7 +20,7 @@ c|cut)
     fname=${4%.*}
     ftype=${4##*.}
     ftime=$(echo $2_$3 | tr : -)
-    ffmpeg -ss $2 -t $3 -safe -1 -i $4 -acodec copy -vcodec copy $fname.$ftime.$ftype -v fatal
+    ffmpeg -ss $2 -t $3 -i $4 -acodec copy -vcodec copy $fname.$ftime.$ftype -v fatal
     ;;
 
 j|join)
